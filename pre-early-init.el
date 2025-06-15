e79d96f3067b3a6b53203691c3a62e98bfa9f72a
Tom Vaughan <thomas.david.vaughan@gmail.com>
2025-06-15 12:37:51 -0400

    WIP:

---
+++ pre-early-init.el
@@ -0,0 +1,12 @@
+;;; FILENAME.el --- DESCRIPTION -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(setq minimal-emacs-user-directory "~/.minimal-emacs+.d/")
+
+;; For users of straight.el.
+(setq package-enable-at-startup nil)
+
+;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
+;; IMPORTANT: This part should be in the pre-early-init.el file
+(setq minimal-emacs-var-dir (expand-file-name "var/" minimal-emacs-user-directory))
+(setq package-user-dir (expand-file-name "elpa" minimal-emacs-var-dir))
+(setq user-emacs-directory minimal-emacs-var-dir)
