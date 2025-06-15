04c9951ab603210e203334c1cc174d70a4ac647d
Tom Vaughan <thomas.david.vaughan@gmail.com>
2025-06-15 12:37:51 -0400

    WIP:

---
+++ pre-early-init.el
@@ -0,0 +1,5 @@
+;;; FILENAME.el --- DESCRIPTION -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(setq minimal-emacs-user-directory "~/.minimal-emacs+.d/")
+(setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
+(setq package-user-dir (expand-file-name "elpa/" user-emacs-directory))
