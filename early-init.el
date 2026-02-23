40a9131f6652962aff3412455e0c157e814ce091
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-02-23 09:39:35 -0500

    Change use-package-always-ensure to (not noninteractive)

--- early-init.el
+++ early-init.el
@@ -455,7 +455,7 @@ this stage of initialization."
 
 (setq use-package-minimum-reported-time (if minimal-emacs-debug 0 0.1))
 (setq use-package-verbose minimal-emacs-debug)
-(setq use-package-always-ensure t)
+(setq use-package-always-ensure (not noninteractive))
 (setq use-package-enable-imenu-support t)
 
 ;; package.el
